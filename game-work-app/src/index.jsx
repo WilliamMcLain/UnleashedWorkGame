7cc16f0a00aa4a11d354d79213e9f5ca6c603d6b
wdmclain <wdmclain@ncsu.edu>
2023-01-08 14:20:22 -0500

    all update

--- game-work-app/src/index.jsx
+++ game-work-app/src/index.jsx
@@ -1,15 +1,33 @@
-import ForgeUI, { render, QueuePage, Fragment, Text } from '@forge/ui';
+import api, { route } from "@forge/api";
+import ForgeUI, { render, Fragment, Text, Macro, useProductContext, useState } from '@forge/ui';
+
+const fetchCommentsForContent = async (contentId) => {
+  const res = await api
+    .asUser()
+    .requestConfluence(route`/wiki/rest/api/content/${contentId}/child/comment`);
+
+  const data = await res.json();
+  return data.results;
+};
 
 const App = () => {
-    return (
-        <Fragment>
-            <Text>Hello world!</Text>
-        </Fragment>
-    );
+  const context = useProductContext();
+  const [comments] = useState(async () => await fetchCommentsForContent(context.contentId));
+
+  console.log(`Number of comments on this page: ${comments.length}`);
+
+  return (
+    <Fragment>
+      <Text>Hello world!</Text>
+      <Text>
+        Number of comments on this page: {comments.length}
+      </Text>
+    </Fragment>
+  );
 };
 
 export const run = render(
-    <QueuePage>
-        <App />
-    </QueuePage>
+  <Macro
+    app={<App />}
+  />
 );
